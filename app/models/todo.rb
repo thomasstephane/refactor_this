fffe73b606c08746a5e72dc8178ec85dc5c1890e
Stephane Thomas <thomas.stephn@gmail.com>
2013-06-12 17:34:05 -0700

    Refactored the model. Merged all the status into a frozen hash.
    created dynamicaly the associated methods
    modified:   app/models/todo.rb

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,122 +1,42 @@
 class Todo < ActiveRecord::Base
+
   attr_accessible :title, :body, :list_name, :todo_count, :status
 
-  def self.grouped_by_list_names
-    self.all.group_by(&:list_name)
-  end
+  STATUS = {
+    :incomplete   => 0,
+    :complete     => 1,
+    :in_progress  => 2,
+    :moved        => 3,
+    :deleted      => 4,
+    :postponed    => 5,
+    :important    => 6
+  }.freeze
 
-  def incomplete?
-    self.status == 0
-  end
-
-  def complete?
-    self.status == 1
-  end
-
-  def in_progress?
-    self.status == 2
-  end
-
-  def moved?
-    self.status == 3
-  end
-
-  def deleted?
-    self.status == 4
-  end
-
-  def postponed?
-    self.status == 5
-  end
-
-  def important?
-    self.status == 6
-  end
-
-  def incomplete!
-    self.update_attributes :status => 0
-  end
-
-  def complete!
-    self.update_attributes :status => 1
-  end
-
-  def in_progress!
-    self.update_attributes :status => 2
-  end
-
-  def moved!
-    self.update_attributes :status => 3
-  end
-
-  def deleted!
-    self.update_attributes :status => 4
-  end
-
-  def postponed!
-    self.update_attributes :status => 5
-  end
-
-  def important!
-    self.update_attributes :status => 6
+  STATUS.keys.each do |status|
+    define_method "#{status}?" do
+      self.status == STATUS[status]
+    end
+    define_method "#{status}!" do
+      update_attributes :status => STATUS[status]
+    end
   end
 
   class << self
-    def all_incomplete
-      self.where :status => 0
+
+    def grouped_by_list_names
+      self.all.group_by(&:list_name)
     end
 
-    def all_complete
-      self.where :status => 1
+    def method_missing(method, args)
+      if method.to_s =~ /all_(.+)$/ && status == STATUS[1$.to_sym]
+        self.where :status => status
+      elsif method.to_s =~ /find_or_create_by_(.+)$/ && status == STATUS[$1.to_sym]
+        self.create :status => status
+      else
+        super
+      end
     end
-
-    def all_in_progress
-      self.where :status => 2
-    end
-
-    def all_moved
-      self.where :status => 3
-    end
-
-    def all_deleted
-      self.where :status => 4
-    end
-
-    def all_postponed
-      self.where :status => 5
-    end
-
-    def all_important
-      self.where :status => 6
-    end
-
-    def create_by_incomplete
-      self.create :status => 0
-    end
-
-    def create_by_complete
-      self.create :status => 1
-    end
-
-    def create_by_in_progress
-      self.create :status => 2
-    end
-
-    def create_by_moved
-      self.create :status => 3
-    end
-
-    def create_by_deleted
-      self.create :status => 4
-    end
-
-    def create_by_postponed
-      self.create :status => 5
-    end
-
-    def create_by_important
-      self.create :status => 6
-    end
+    
   end
 
 end
